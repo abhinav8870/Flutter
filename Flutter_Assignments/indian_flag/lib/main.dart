e7e8f13949362f965a9b142b270847165552faad
abhinav8870 <abhinavbhadge14@gmail.com>
2024-09-27 14:15:10 +0530

    Indian_Flag

---
+++ Flutter_Assignments/indian_flag/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Indian Flag",
+      home: MyScreen(),
+    );
+  }
+}
+
+class MyScreen extends StatelessWidget {
+  const MyScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Indian Flag"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              height: 45,
+              color: Colors.orange,
+            ),
+            Container(
+              width: 300,
+              height: 45,
+              color: Colors.white,
+              child: Image.network(
+                  "https://upload.wikimedia.org/wikipedia/commons/e/e4/Spinning_Ashoka_Chakra.gif"),
+            ),
+            Container(
+              width: 300,
+              height: 45,
+              color: Colors.green,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
