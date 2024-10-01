5707674db7cae316ed012b6c7802e48d73dd8c0d
abhinav8870 <abhinavbhadge14@gmail.com>
2024-10-01 23:24:02 +0530

    Revert "Apps"
    This reverts commit 857f81607216c06d1de23faad73bf4b66cdad76f.

--- Flutter_Assignments/container_colorchange/android/app/src/main/kotlin/com/example/container_colorchange/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.container_colorchange
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Flutter_Assignments/indian_flag/lib/main.dart
+++ Flutter_Assignments/indian_flag/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "Indian Flag",
       home: MyScreen(),
     );
